f63a2945cd95d659f31a39668cdb774214c95682
fedepal <fedepalmero@gmail.com>
2015-08-26 00:34:48 -0300

    Agregado corpus. Modificado train.py para trabajar con ese corpus

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -12,7 +12,7 @@ Options:
 from docopt import docopt
 import pickle
 
-from nltk.corpus import gutenberg
+from nltk.corpus import PlaintextCorpusReader
 
 from languagemodeling.ngram import NGram
 
@@ -21,7 +21,8 @@ if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = gutenberg.sents('austen-emma.txt')
+    sents = PlaintextCorpusReader('../../corpus','.*\.txt').sents()
+    #sents = gutenberg.sents('austen-emma.txt')
 
     # train the model
     n = int(opts['-n'])
