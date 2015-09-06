3a44b30bc91659f79cbc9d1cd18e2ce21afee2ee
Federico Palmero <fedepalmero@gmail.com>
2015-09-06 01:26:28 -0300

    Correcciones en el corpus. train.py carga el corpus utilizando regexp. Ngram.py OK pasa los test(Ej2)

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -12,17 +12,29 @@ Options:
 from docopt import docopt
 import pickle
 
+from nltk import RegexpTokenizer
 from nltk.corpus import PlaintextCorpusReader
-
+from nltk.data import LazyLoader
 from languagemodeling.ngram import NGram
-
+import os.path
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
+    pattern = r'''(?ix)    # set flag to allow verbose regexps
+            sr(es)?\. | sras?\. | etc\.
+            | ([A-Z]\.)+        # abbreviations, e.g. U.S.A.
+            | \w+(-\w+)*        # words with optional internal hyphens
+            | \$?\d+(\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
+            | \.\.\.            # ellipsis
+            | [][.,;"'?():-_`]  # these are separate tokens; includes ], [
+            '''
+    tokenizer = RegexpTokenizer(pattern)
+
     # load the data
-    sents = PlaintextCorpusReader('../../corpus','.*\.txt').sents()
-    #sents = gutenberg.sents('austen-emma.txt')
+    sents = PlaintextCorpusReader('../../corpus','.*\.txt',word_tokenizer=tokenizer,
+                                sent_tokenizer=LazyLoader('tokenizers/punkt/spanish.pickle')).sents()
+
 
     # train the model
     n = int(opts['-n'])
