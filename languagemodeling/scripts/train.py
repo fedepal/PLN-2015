37b856a133fb5b768cc31d393817a4d9e7e3099b
Federico Palmero <fedepalmero@gmail.com>
2015-09-12 01:40:07 -0300

    Listo AddOneNGram Tests OK. Agregado eval.py y calculo de perplexity. Modificado train.py para elegir AddOne. Una parte de Interpolated falta testear

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,11 +1,14 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> -o <file>
+  train.py -n <n> [-m <model>] -o <file>
   train.py -h | --help
 
 Options:
   -n <n>        Order of the model.
+  -m <model>    Model to use [default: ngram]:
+                  ngram: Unsmoothed n-grams.
+                  addone: N-grams with add-one smoothing.
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
@@ -15,7 +18,7 @@ import pickle
 from nltk import RegexpTokenizer
 from nltk.corpus import PlaintextCorpusReader
 from nltk.data import LazyLoader
-from languagemodeling.ngram import NGram
+from languagemodeling.ngram import NGram, AddOneNGram
 import os.path
 
 if __name__ == '__main__':
@@ -27,7 +30,7 @@ if __name__ == '__main__':
             | \w+(-\w+)*        # words with optional internal hyphens
             | \$?\d+(\.\d+)?%?  # currency and percentages, e.g. $12.40, 82%
             | \.\.\.            # ellipsis
-            | [][.,;"'?():-_`]  # these are separate tokens; includes ], [
+            | [][.,;"'?():-_`]  # these are separate tokens; includes ],
             '''
     tokenizer = RegexpTokenizer(pattern)
 
@@ -35,10 +38,20 @@ if __name__ == '__main__':
     sents = PlaintextCorpusReader('../../corpus','.*\.txt',word_tokenizer=tokenizer,
                                 sent_tokenizer=LazyLoader('tokenizers/punkt/spanish.pickle')).sents()
 
+    #slice data 90% train data
+    sents = sents[:-int(0.1*len(sents))]
 
     # train the model
     n = int(opts['-n'])
-    model = NGram(n, sents)
+    m = (opts['-m'])
+
+    if m == "ngram":
+        model = NGram(n, sents)
+    elif m == "addone":
+        model = AddOneNGram(n, sents)
+    else:
+        print(__doc__)
+        exit(0)
 
     # save it
     filename = opts['-o']
