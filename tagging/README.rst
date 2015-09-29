d9e3821258a565bfb0ecd2a2602d955c151ab6d6
Federico Palmero <fedepalmero@gmail.com>
2015-09-29 00:52:46 -0300

    Listo ejercicio 1

---
+++ tagging/README.rst
@@ -0,0 +1,43 @@
+Readme Práctico 2 - Etiquetado de Secuencias
+============================================
+
+Ejercicio 1
+-----------
+
+**sents:** 17379
+**ocurrences:** 517300
+**V:** 49071
+**K:** 48
+
+Frecuencia de tags
+^^^^^^^^^^^^^^^^^^
+=== =====   ======  =======================================
+tag freq    %       top5
+=== =====   ======  =======================================
+nc  92002   17.79%  años presidente millones equipo partido
+sp  79904   15.45%  de en a del con
+da  54552   10.55%  la el los las El
+vm  50609   9.78%   está tiene dijo puede hace
+aq  33904   6.55%   pasado gran mayor nuevo próximo
+fc  30148   5.83%   ,
+np  29113   5.63%   Gobierno España PP Barcelona Madrid
+fp  21157   4.09%   . ( )
+rg  15333   2.96%   más hoy también ayer ya
+cc  15023   2.90%   y pero o Pero e
+=== =====   ======  =======================================
+
+Ambigüedad
+^^^^^^^^^^
+=   =====   ====== =====================
+n   words   %      examples
+=   =====   ====== =====================
+1   44110   89.89% , el en con por
+2   2194    4.47%  la y " los del
+3   153     0.31%  . a un no es
+4   19      0.04%  de dos este tres todo
+5   4       0.01%  que mismo cinco medio
+6   3       0.01%  una como uno
+7   0       0.00%
+8   0       0.00%
+9   0       0.00%
+=   =====   ====== =====================
--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -11,7 +11,7 @@ from docopt import docopt
 
 from corpus.ancora import SimpleAncoraCorpusReader
 from itertools import chain
-
+from collections import defaultdict, Counter
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
@@ -22,23 +22,84 @@ if __name__ == '__main__':
 
     # compute the statistics
     print('sents: {}'.format(len(sents)))
-    #compute ocurrences
 
+    #compute ocurrences
     sents = list(chain.from_iterable(sents))
     words = defaultdict(int)
     tags = defaultdict(int)
     for w, t in sents:
-        words[w] += 1
+        words[(w,t)] += 1
         tags[t] += 1
+
     ocurrences = sum(words.values())
+    print('ocurrences: {}'.format(ocurrences))
     # compute vocabulary
     v = len(words)
+    print('V: {}'.format(v))
     # compute tags vocabulary
-    s = len(tags)
+    k = len(tags)
+    print('K: {}'.format(k))
 
     # Tags more frecuents
 
-    
+    words = sorted(words.items(), key=lambda x: x[1], reverse=True)
+    tags = sorted(tags.items(), key=lambda x: x[1], reverse=True)
+
+    freq = defaultdict(tuple)
+    for tag in tags[:10]:
+        i = 0
+        while len(freq[tag[0]]) < 5 and i < len(words):
+            if words[i][0][1] == tag[0]:
+                freq[tag[0]] += (words[i][0][0],)
+            i += 1
+    print("\nTags")
+    print('tag\tfreq\t%\ttop5')
+    i = 0
+    while i in range(0,10):
+        print('{0}\t{1}\t{2:2.2%}\t{3}\t'.format(tags[i][0],tags[i][1],tags[i][1]/ocurrences,' '.join(freq[tags[i][0]])))
+        i += 1
+
+    #ambiguity
+
+    t1, t2 = zip(*words)  # t1 (x1,x2), x1 = word, x2 = tag t2 = (counts)
+    a1, a2 = zip(*t1)  # a1: Words, a2: tags, words with repeats(repetitions)
+    t1 = None
+    a2 = None
+    words_counts = zip(a1,t2)
+    t2 = None
+    c = Counter(a1)  # Cuenta palabras repetidas en a1
+    a1 = None
+    amb = Counter(c.values())  # Cuenta cantidad de repeticiones de palabras
+
+    l_amb = defaultdict(lambda : ('',0,0))
+    for w,co in words_counts:
+        l_amb[w] = (w,l_amb[w][1] + co, c[w])
+    l_amb = l_amb.values()
+
+    l_amb = sorted(l_amb, key=lambda x:(x[2],x[1]),reverse=True)
+
+    # Ya ordenadas por frecuencia y ambiguedad, deshacemos de los counts
+
+    l_amb, a, b = zip(*l_amb)
+    a = None
+    b = None
+    result = defaultdict(tuple)
+    i = 10
+    index = 0
+    while len(result) < 10:  # FIX las palabras no son las más frecuentes
+        i -= 1
+        index += amb[i]
+        result[i]
+        if index != 0 and amb[i]>=5:
+            result[i] = tuple(l_amb[:5])
+        else:
+            result[i] = tuple(l_amb[0:amb[i]])
+        l_amb = l_amb[amb[i]:]
+    print("\nWords")
+    print('n\twords\t%\texamples')
+    for i in range(1,10):
+        print('{0}\t{1}\t{2:2.2%}\t{3}'.format(i,amb[i],amb[i]/v,' '.join(result[i])))
+
 
 # l1,l2 = zip(*c)
 #*t desarma tuplas
