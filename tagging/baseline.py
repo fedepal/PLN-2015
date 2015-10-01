89fc5a99986e2124c2c107f0b1919dc8bfbf846a
Federico Palmero <fedepalmero@gmail.com>
2015-10-01 14:38:40 -0300

    Ejercicio 2, baseline.py. Pasa los tests

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,4 +1,5 @@
-
+from collections import defaultdict
+from itertools import chain
 
 class BaselineTagger:
 
@@ -6,7 +7,19 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        tagged_sents = list(chain.from_iterable(tagged_sents))
+        tag_sents = defaultdict(str)
+        tags = defaultdict(int)
+
+        for w,t in tagged_sents:  # Ver que pasa en el caso de ambiguedad
+            tag_sents[w] = t
+        self.tag_sents = dict(tag_sents)
+
+        # Calculate most frequent tag
+        for w, t in tagged_sents:
+            tags[t] += 1
+        tags = sorted(tags.items(), key=lambda x: x[1], reverse=True)
+        self.tag_frequent = tags[0][0]
 
     def tag(self, sent):
         """Tag a sentence.
@@ -20,11 +33,18 @@ class BaselineTagger:
 
         w -- the word.
         """
-        return 'nc'
+        r = self.tag_sents.get(w)
+        if r is None:
+            r = self.tag_frequent
+        return r
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        r = self.tag_sents.get(w,True)
+
+        if r is not True:
+            r = False
+        return r
